4efd26606f7c82e531121cf38015331c41cc5959
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-12-19 16:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
-// Write your code here and print the output using alert function
+const num1 = Number(input1);
+const num2 = Number(input2);
+function addTwoNumber(num1, num2) {
+  if (
+    input1 === null || input2 === null ||
+    input1.trim() === "" || input2.trim() === "" ||
+    isNaN(num1) || isNaN(num2)
+  ) {
+    alert("Invalid input. Please enter a valid number.");
+  } else {
+    let sum = num1 + num2;
+    alert(`The sum of ${num1} and ${num2} is ${sum}.`);
+  }
+}
+
+addTwoNumber(num1, num2);
